7439a1134a642e48b35939f29194aaf4d4427c17
ota42y <ota42y@gmail.com>
2014-03-16 14:04:08 +0900

    .emacsの修正その２

--- home/.emacs.d/conf/25-open-junk-file.el
+++ home/.emacs.d/conf/25-open-junk-file.el
@@ -2,7 +2,8 @@
 ;; 読み込み
 (require 'open-junk-file)
 ;; ファイル名およびファイル場所の指定
-(setq open-junk-file-format "~/aa_files/Dropbox/data/emacs/%Y%m/%Y-%m-%d memo.txt")
+(setq open-junk-file-format "~/aa_files/Dropbox/archive/text/junk/%Y%m/%Y-%m-%d memo.txt")
+
 (global-set-key (kbd "C-,") 'open-junk-file)
 ; ターミナルだとC-,が遅れないのでF11に割り当てる
 (global-set-key (kbd "<f11>") 'open-junk-file)
