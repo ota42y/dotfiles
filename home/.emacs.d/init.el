44c01fafe79293a168c6c976e9e8a09e69d92685
ota42y <ota42y@gmail.com>
2015-06-23 22:18:56 +0900

    add emacs client setting

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -54,3 +54,12 @@
 (setq browse-url-generic-program
 (if (file-exists-p "/usr/bin/open")
 "/usr/bin/open"))
+
+
+;; run server
+(require 'server)
+(unless (server-running-p)
+(server-start))
+
+(global-set-key (kbd "C-x C-c") 'ns-do-hide-emacs) ;emacs休止
+(defalias 'exit 'save-buffers-kill-emacs) ;M-x exit で emacs終了
